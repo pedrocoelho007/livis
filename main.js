34453e77251a6c2255364346accf9a810cb634be
as vezes me sinto o justin <142445197+pedrocoelho007@users.noreply.github.com>
2024-09-23 10:11:54 -0300

    fd

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(5));
-let areaTriangulo =function(parametro){
+let areaTriangulo =function(base,altura){
     return ;
 }
-console.log(areaTriangulo(36));
+console.log(areaTriangulo());
